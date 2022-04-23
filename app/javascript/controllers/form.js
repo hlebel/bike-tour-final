e93b499fa4928679d92d4d011c25d739dc3c2c46
hlebel <hparalleda@gmail.com>
2022-04-23 18:54:47 +0200

    Fixed turbo reload

---
+++ app/javascript/controllers/form.js
@@ -0,0 +1,67 @@
+const form = document.querySelector("#slogan-form");
+const result = document.querySelector("#result");
+
+if (window.location.pathname === "/slogans") {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    let data = {};
+    for (const [key, value] of formData.entries()) {
+      data[key] = value;
+      // data = {...data, [key]: value}
+    }
+
+    document.querySelectorAll("input").forEach((input) => {
+      input.classList.remove("is-invalid");
+      input.nextElementSibling.innerText = "";
+    });
+
+    const errors = {};
+    if (!data.first_name) errors.first_name = "Must enter your first name";
+    if (!data.last_name) errors.last_name = "Must enter your last name";
+    if (!data.email) errors.email = "Must enter your email";
+    if (!isEmailValid(data.email)) errors.email = "Invalid email";
+    if (!data.suggestion) errors.suggestion = "Must enter a slogan";
+    if (data.suggestion.length > 50)
+      errors.suggestion = "Slogan cannot exceed 50 characters";
+
+    if (Object.keys(errors).length > 0) {
+      for (const error of Object.keys(errors)) {
+        const input = document.querySelector(`#${error}`);
+        input.classList.add("is-invalid");
+        input.nextElementSibling.innerText = errors[error];
+      }
+    } else {
+      let res = await fetch(`/api/slogans`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          // 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+          "Content-Type": "application/json",
+        },
+      });
+
+      res = await res.json();
+
+      if (res.status === "ok") {
+        result.innerHTML = `<div class="alert alert-success">${res.message}</div>`;
+      } else {
+        result.innerHTML = `<div class="alert alert-danger">Oops! something went wrong.</div>`;
+      }
+
+      form.reset();
+
+      console.log({ res });
+    }
+  });
+}
+
+function isEmailValid(email) {
+  const emailRegexp = new RegExp(
+    /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i
+  );
+
+  return emailRegexp.test(email);
+}
---
+++ app/javascript/controllers/gallery.js
@@ -0,0 +1,35 @@
+const root = document.querySelector("#root");
+const loader = document.querySelector("#loader");
+
+if (root) {
+  const FLICKR_API_KEY = "f71a0d235c15d5af6b9034530a160c9b";
+
+  let page = 1;
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.25,
+  };
+
+  const fetchPhotos = async (page) => {
+    const res = await fetch(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&tags=bikerace%2C+BoulderBikeTour&safe_search=1&content_type=1&media=photos&geo_context=2&per_page=12&page=${page}&format=json&nojsoncallback=1`
+    );
+    return await res.json();
+  };
+
+  const handleIntersect = async (entries, observer) => {
+    if (entries[0].isIntersecting) {
+      const { photos } = await fetchPhotos(page);
+      page++;
+
+      photos.photo.map((u) => {
+        root.innerHTML += `<img src="https://farm${u.farm}.staticflickr.com/${u.server}/${u.id}_${u.secret}.jpg"/>`;
+      });
+    }
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(loader);
+}
